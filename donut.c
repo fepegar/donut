cced3565b9772a7e334cad235985b6d668e8b000
Fernando Pérez-García <fepegar@gmail.com>
2021-12-20 23:54:37 +0000

    Rename some more variables

--- donut.c
+++ donut.c
@@ -6,6 +6,7 @@
 #define DELTA_DEGREES_Z 0.1
 #define NEWLINE 10
 #define CLEAR_SCREEN "\x1b[2J"
+#define SPACE 32
 #define RETURN_CURSOR "\x1b[H"
 #define DELTA_CIRCLE 1.15
 #define DELTA_TORUS 4
@@ -13,7 +14,9 @@
 #define NUM_COLS 80
 #define BUFFER_SIZE NUM_ROWS * NUM_COLS
 #define Z_BUFFER_SIZE NUM_ROWS * NUM_COLS * 4
-
+#define Z_OFFSET 5
+#define X_OFFSET NUM_COLS / 2
+#define Y_OFFSET NUM_ROWS / 2
 
 
 int main(void){
@@ -33,7 +36,7 @@ int main(void){
   printf(CLEAR_SCREEN);
 
   while(1){
-    memset(frameBuffer, 32, BUFFER_SIZE);
+    memset(frameBuffer, SPACE, BUFFER_SIZE);
     memset(zBuffer, 0, Z_BUFFER_SIZE);
     sinX = sin(radsX);
     cosX = cos(radsX);
@@ -48,16 +51,21 @@ int main(void){
         sinCircle = sin(angleCircle);
         cosCircle = cos(angleCircle);
         float h = cosTorus + 2;
-        float D = 1 / (sinCircle * h * sinX + sinTorus * cosX + 5);
+        float D = 1 / (sinCircle * h * sinX + sinTorus * cosX + Z_OFFSET);
         float t = sinCircle * h * cosX - sinTorus * sinX;
-        int x = 40 + 30 * D * (cosCircle * h * cosZ - t * sinZ);
-        int y = 12 + 15 * D * (cosCircle * h * sinZ + t * cosZ);
-        int o = x + 80 * y;  // 80 = NUM_COLS?
-        int N = 8 * ((sinTorus * sinX - sinCircle * cosTorus * cosX) * cosZ - sinCircle * cosTorus * sinX - sinTorus * cosX - cosCircle * cosTorus * sinZ);
-        if(y < NUM_ROWS && y > 0 && x > 0 && x < NUM_COLS && D > zBuffer[o]){
-          zBuffer[o] = D;
+        int x = X_OFFSET + 30 * D * (cosCircle * h * cosZ - t * sinZ);
+        int y = Y_OFFSET + 15 * D * (cosCircle * h * sinZ + t * cosZ);
+        bufferIdx = x + NUM_COLS * y;  // 80 = NUM_COLS?
+        int N = 8 * (
+          (sinTorus * sinX - sinCircle * cosTorus * cosX) * cosZ
+          - sinCircle * cosTorus * sinX
+          - sinTorus * cosX
+          - cosCircle * cosTorus * sinZ
+        );
+        if(y < NUM_ROWS && y > 0 && x > 0 && x < NUM_COLS && D > zBuffer[bufferIdx]){
+          zBuffer[bufferIdx] = D;
           charIndex = N > 0 ? N : 0;
-          frameBuffer[o] = ".,-~:;=!*#$@"[charIndex];
+          frameBuffer[bufferIdx] = ".,-~:;=!*#$@"[charIndex];
         }
       }
     }
